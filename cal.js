c0b2adef49e07d6f9d54d6c1352d8119d2ace364
sakhiimam3 <sakhiimam3411@gmail.com>
2020-11-14 22:30:41 +0500

    first commit

---
+++ cal.js
@@ -0,0 +1,21 @@
+
+function getNum(num){
+    var result=document.getElementById("result");
+    result.value +=num
+}
+function clearRes(num){
+    var result=document.getElementById("result");
+    result.value=""
+}
+function abc(){
+    var result=document.getElementById("result");
+    result.value=eval(result.value);
+}
+
+
+
+function make() {
+     document.getElementById("pic").className="hidden"
+}
+
+
